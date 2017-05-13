fcc9ce51527fe1735be789fd8570ef3dcc7ebb6a
Jay Chang <jay.cychang@gmail.com>
2017-05-12 22:33:47 -0500

    Initial test using Stichoza\GoogleTranslate package

--- src/hello.php
+++ src/hello.php
@@ -1,3 +1,12 @@
 <?php
+namespace src;
+require_once __DIR__ . '/../vendor/autoload.php';
 
-    echo "Hello Texan!!";
+use Stichoza\GoogleTranslate\TranslateClient;
+
+$tr = new TranslateClient(); // Default is from 'auto' to 'en'
+$tr->setSource('en'); // Translate from English
+$tr->setTarget('es'); // Translate to Spanish
+
+echo $tr->translate('Hello Texan!');
+echo "\n";
